9b94795733cddc6a7ab084c13be33a1b9f5feb47
Sebastian Hennebrueder <usenet@laliluna.de>
2014-11-06 22:14:37 +0100

    add lein midje to plugin dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.mongodb/mongo-java-driver "2.12.1"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.json "0.2.4"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
