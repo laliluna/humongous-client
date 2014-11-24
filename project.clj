a8ba95d0dbe3607d3c82042b0e655e7a69fea154
Sebastian Hennebrueder <usenet@laliluna.de>
2014-11-24 11:59:50 +0100

    lein-release plugin: bumped version from 0.0.2 to 0.0.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject humongous-client "0.0.2"
+(defproject humongous-client "0.0.3-SNAPSHOT"
   :description "Mongo DB client"
   :url "https://github.com/laliluna/humongous-client"
   :license {:name "Apache License Version 2.0"
