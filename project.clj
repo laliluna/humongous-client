eb09be7c40ea4a5fbec3d35d3314114ce1dc9b93
Sebastian Hennebrueder <usenet@laliluna.de>
2017-08-13 17:47:15 +0200

    Bump up java driver version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.apache.org/licenses/"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.mongodb/mongo-java-driver "2.12.1"]
+                 [org.mongodb/mongo-java-driver "2.13.2"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.json "0.2.4"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
@@ -16,4 +16,3 @@
   :aliases {"test-all"
             ["with-profile" "dev,1.5:dev,1.6" "midje"]}
   :deploy-repositories [["releases" :clojars]])
-  
