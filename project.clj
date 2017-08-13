deaf188b412872c7006e575a5c85c1933d2dbfad
Sebastian Hennebrueder <usenet@laliluna.de>
2017-08-13 20:58:19 +0200

    disable signing

--- project.clj
+++ project.clj
@@ -1,19 +1,29 @@
-(defproject humongous-client "0.0.3-SNAPSHOT"
-  :description "Mongo DB client"
-  :url "https://github.com/laliluna/humongous-client"
-  :license {:name "Apache License Version 2.0"
-            :url "http://www.apache.org/licenses/"}
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.mongodb/mongo-java-driver "2.13.2"]
-                 [org.clojure/core.incubator "0.1.3"]
-                 [org.clojure/data.json "0.2.4"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+(defproject
+ humongous-client "0.0.3-SNAPSHOT"
+ :description "Mongo DB client"
+ :url "https://github.com/laliluna/humongous-client"
+ :license {:name "Apache License Version 2.0"
+           :url "http://www.apache.org/licenses/"}
+ :min-lein-version "2.0.0"
+ :dependencies [[org.clojure/clojure "1.6.0"]
+                [org.mongodb/mongo-java-driver "2.13.2"]
+                [org.clojure/core.incubator "0.1.3"]
+                [org.clojure/data.json "0.2.4"]]
+ :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                  :plugins [[lein-midje "3.1.3"] [lein-cprint "1.2.0"]]}
+            :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+            :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+            :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
 
-  :aliases {"test-all"
-            ["with-profile" "dev,1.6:dev,1.7:dev,1.8" "midje"]}
-  :deploy-repositories [["releases" :clojars]])
+ :aliases {"test-all"
+           ["with-profile" "dev,1.6:dev,1.7:dev,1.8" "midje"]}
+ :deploy-repositories [["releases" :clojars]]
+ :release-tasks
+ [["vcs" "assert-committed"]
+  ["change" "version" "leiningen.release/bump-version" "release"]
+  ["vcs" "commit"]
+  ["vcs" "tag" "--no-sign"]
+  ["deploy"]
+  ["change" "version" "leiningen.release/bump-version"]
+  ["vcs" "commit"]
+  ["vcs" "push"]])
